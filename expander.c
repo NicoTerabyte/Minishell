c4075ebe23c13b945823ac262a7fdac1531d678c
Alessio Buonomo <abuonomo@e4r12p1.42roma.it>
2023-08-30 16:59:25 +0200

    Aggiunte anche le quotes dovrebberofarsi dei test

--- expander.c
+++ expander.c
@@ -6,7 +6,7 @@
 /*   By: abuonomo <abuonomo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:35:17 by abuonomo          #+#    #+#             */
-/*   Updated: 2023/08/29 18:24:19 by abuonomo         ###   ########.fr       */
+/*   Updated: 2023/08/30 16:58:52 by abuonomo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,14 @@ char *find_in_env(t_data *shell_data, char *input)
 	}
 	while (*input)
 	{
-		if (*input == '$' && *(input + 1) && ft_isalpha(*(input + 1)))
+		if(*input == 39){
+			input++;
+			while(*input != 39)
+			{
+				ret[ret_index++] = *input++;
+			}
+			input++;
+		}else if (*input == '$' && *(input + 1) && ft_isalpha(*(input + 1)))
 		{
 			input++;
 			var_index = 0; // Indice per la posizione corrente nel nome della variabile
@@ -62,6 +69,34 @@ char *find_in_env(t_data *shell_data, char *input)
 				}
 				i++;
 			}
+		}
+		else if(*input == 34){
+			input++;
+			while(*input != 34)
+			{
+				if (*input == '$' && *(input + 1) && ft_isalpha(*(input + 1)))
+		{
+			input++;
+			var_index = 0; // Indice per la posizione corrente nel nome della variabile
+			while (ft_isalnum(*input) || *input == '_')
+				var[var_index++] = *input++;
+			var[var_index] = '\0'; // Termina la stringa di nome variabile
+			i = 0;
+			while (shell_data->copy_env[i])
+			{
+				if (ft_strcmp_env(var, shell_data->copy_env[i]) == 0)
+				{
+					value_start = strchr(shell_data->copy_env[i], '=') + 1;
+					while (*value_start)
+						ret[ret_index++] = *value_start++; // Copia il valore della variabile nell'output
+					break;
+				}
+				i++;
+			}
+		}
+				ret[ret_index++] = *input++;
+			}
+			input++;
 		}
 		else
 			ret[ret_index++] = *input++; // Copia il carattere normale nell'output
