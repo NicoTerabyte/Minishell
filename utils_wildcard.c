f2839d5136f0d1f243d7cd7a87c300d1b3736b10
Alessio Buonomo <abuonomo@e4r8p3.42roma.it>
2023-09-13 17:34:41 +0200

    Added Correction

--- utils_wildcard.c
+++ utils_wildcard.c
@@ -6,67 +6,70 @@
 /*   By: abuonomo <abuonomo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 17:52:51 by abuonomo          #+#    #+#             */
-/*   Updated: 2023/09/13 15:01:25 by abuonomo         ###   ########.fr       */
+/*   Updated: 2023/09/13 16:27:30 by abuonomo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int is_wildcard(char *input)
+int	is_wildcard(char *input)
 {
-	while(*input){
-		if(*input == '*')
-			return 1;
+	while (*input)
+	{
+		if (*input == '*')
+			return (1);
 		input++;
 	}
-	return 0;
+	return (0);
 }
 
-int matrix_dimension(t_data *shell_data)
+char	*current_dir(void)
 {
+	char	*cd;
 
-	DIR *dir = opendir(shell_data->copy_env[21]);
-	struct dirent *entry;
-	int count;
-	if (dir == NULL) {
-		perror("opendir() error");
-		return 1;
+	cd = malloc(1024 * sizeof(char));
+	if (getcwd(cd, 1024) == NULL)
+		perror("Errore ottenimento del percorso");
+	return (cd);
+}
+
+int	matrix_dimension(t_data *shell_data)
+{
+	DIR				*dir;
+	struct dirent	*entry;
+	int				count;
+
+	count = 0;
+	dir = opendir(current_dir());
+	entry = readdir(dir);
+	while (entry != NULL)
+	{
+		entry = readdir(dir);
+		count++;
 	}
-	while ((entry = readdir(dir)) != NULL)
-			count++;
-	return count;
 	closedir(dir);
+	return (count);
 }
 
-void stampaMatriceChar(char** matrice) {
-    for (int i = 0; matrice[i] != NULL; i++) {
-        for (int j = 0; matrice[i][j] != '\0'; j++) {
-            printf("%c ", matrice[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-
-char **matrix_dir(t_data *shell_data)
+char	**matrix_dir(t_data *shell_data)
 {
-	int		rows;
-	char	**dir_matrix;
-	struct	dirent *entry;
-	DIR		*dir;
-	dir = opendir(shell_data->copy_env[21]);
+	int				rows;
+	char			**dir_matrix;
+	struct dirent	*entry;
+	DIR				*dir;
+
+	dir = opendir(current_dir());
 	rows = matrix_dimension(shell_data);
-	dir_matrix = (char **) malloc(rows*sizeof(char *));
+	dir_matrix = (char **) malloc((rows + 1) * sizeof(char *));
 	rows = 0;
-	if (dir == NULL) {
-		perror("opendir() error");
-		return 1;
+	entry = readdir(dir);
+	while (entry != NULL)
+	{
+		entry = readdir(dir);
+		dir_matrix[rows++] = entry->d_name;
 	}
-	while ((entry = readdir(dir)) != NULL)
-		dir_matrix[rows++][0] = entry->d_name;
-	stampaMatriceChar(dir_matrix);
+	dir_matrix[rows] = 0;
+	stampaMatrice(dir_matrix);
 	closedir(dir);
-	return dir_matrix;
+	return (dir_matrix);
 }
-
-
--- wildcard.c
+++ wildcard.c
@@ -6,95 +6,72 @@
 /*   By: abuonomo <abuonomo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:35:17 by abuonomo          #+#    #+#             */
-/*   Updated: 2023/09/12 18:29:52 by abuonomo         ###   ########.fr       */
+/*   Updated: 2023/09/13 17:27:06 by abuonomo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char *extension(t_data *shell_data, char *wildstring)
+//Searching File with Particular Extension and ‘*’
+	//You can search any file by using the asterisk (*) and the file extension.
+	//If you want to search all files with the ‘.txt’ extension from the current directory
+	//then run the following command from the terminal. Here, the filename can be any
+	//character(s) and any number of characters.
+char	*extension(t_data *shell_data, char *wildstring)
 {
-	return wildstring;
+	shell_data->dir_list
+	return (wildstring);
 }
 
 //Searching Specific File with Filename and ‘*’
 	//‘ls’ command is used to find out the list of files and folders of the current directory.
 	//‘ls a*’ command will search and print all filenames of the current directory that
 	//starts with the character, ‘a’.
-
-
-char *filename(t_data *shell_data, char *wildstring){
-
-	return wildstring;
+char	*filename(t_data *shell_data, char *wildstring)
+{
+	return (wildstring);
 }
-
-
-char *partial(t_data *shell_data, char *wildstring){
-	return wildstring;
-}
-
-
-
-
-
-//Searching File with Particular Extension and ‘*’
-	//You can search any file by using the asterisk (*) and the file extension.
-	//If you want to search all files with the ‘.txt’ extension from the current directory
-	//then run the following command from the terminal. Here, the filename can be any
-	//character(s) and any number of characters.
 
 //Removing File by Partial Match and ‘*’
 	//You can use the asterisk (*) for matching any filename partially. The following command
 	//will remove the file which contains the ‘test’ word in any part of the filename.
+char	*partial(t_data *shell_data, char *wildstring)
+{
+	return (wildstring);
+}
 
+char	*translate_wild(t_data *shell_data, char *input){
 
-char *translate_wild(t_data *shell_data, char *input){
+	char	*wildstring;
+	int		i;
+	int		x;
 
-	char *wildstring = malloc(ft_strlen(input) + 1);
-	int i = 0;
-	int x = 0;
-
-	while(input[i])
+	i = 0;
+	x = 0;
+	wildstring = malloc(ft_strlen(input) + 1);
+	while (input[i])
 	{
-		if(input[i] == '*')
+		if (input[i] == '*')
 		{
 			i++;
-			if(input[i] == '.'){
+			if (input[i] == '.')
+			{
 				wildstring[x++] = input[i++];
-				while(ft_isalpha(input[i])){
+				while (ft_isalpha(input[i]))
 					wildstring[x++] = input[i++];
-				}
-				printf("%s\n",wildstring);
 				wildstring[x] = 0;
-				return extension(shell_data, wildstring);
+				return (extension(shell_data, wildstring));
 			}else if(ft_isalpha(input[i]))
 			{
 				while(ft_isalpha(input[i]) || input[i] == '*'){
 					if(input[i] == '*')
-						return partial(shell_data, wildstring);
+						return (partial(shell_data, wildstring));
 					wildstring[x++] = input[i++];
 				}
-				printf("%s\n",wildstring);
 				wildstring[x] = 0;
-				return filename(shell_data, wildstring);
+				return (filename(shell_data, wildstring));
 			}
 		}
 		i++;
 	}
 }
-
-
-//Searching Specific File with Filename and ‘*’
-	//‘ls’ command is used to find out the list of files and folders of the current directory.
-	//‘ls a*’ command will search and print all filenames of the current directory that
-	//starts with the character, ‘a’.
-
-//Searching File with Particular Extension and ‘*’
-	//You can search any file by using the asterisk (*) and the file extension.
-	//If you want to search all files with the ‘.txt’ extension from the current directory
-	//then run the following command from the terminal. Here, the filename can be any
-	//character(s) and any number of characters.
-
-//Removing File by Partial Match and ‘*’
-	//You can use the asterisk (*) for matching any filename partially. The following command
-	//will remove the file which contains the ‘test’ word in any part of the filename.
