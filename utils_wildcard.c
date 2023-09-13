feb42849f2690d21ade08aca0d66dad9934226e1
Alessio Buonomo <abuonomo@e4r8p3.42roma.it>
2023-09-13 15:01:41 +0200

    Aggiunta creazione matrix

--- utils_wildcard.c
+++ utils_wildcard.c
@@ -6,7 +6,7 @@
 /*   By: abuonomo <abuonomo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 17:52:51 by abuonomo          #+#    #+#             */
-/*   Updated: 2023/09/12 18:53:06 by abuonomo         ###   ########.fr       */
+/*   Updated: 2023/09/13 15:01:25 by abuonomo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,17 +38,35 @@ int matrix_dimension(t_data *shell_data)
 	closedir(dir);
 }
 
+void stampaMatriceChar(char** matrice) {
+    for (int i = 0; matrice[i] != NULL; i++) {
+        for (int j = 0; matrice[i][j] != '\0'; j++) {
+            printf("%c ", matrice[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
 char **matrix_dir(t_data *shell_data)
 {
-	int rows;
-	char **dir_matrix;
-	DIR *dir = opendir(shell_data->copy_env[21]);
-
+	int		rows;
+	char	**dir_matrix;
+	struct	dirent *entry;
+	DIR		*dir;
+	dir = opendir(shell_data->copy_env[21]);
 	rows = matrix_dimension(shell_data);
 	dir_matrix = (char **) malloc(rows*sizeof(char *));
+	rows = 0;
 	if (dir == NULL) {
 		perror("opendir() error");
 		return 1;
 	}
+	while ((entry = readdir(dir)) != NULL)
+		dir_matrix[rows++][0] = entry->d_name;
+	stampaMatriceChar(dir_matrix);
 	closedir(dir);
+	return dir_matrix;
 }
+
+
