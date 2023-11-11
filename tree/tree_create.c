c7ee9498129c0a51bac7719558de1669d918c7ae
Manuele Longo <mlongo@e4r4p2.42roma.it>
2023-11-11 10:53:51 +0100

    norminette tree_create.c

--- tree/tree_create.c
+++ tree/tree_create.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tree_create.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abuonomo <abuonomo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mlongo <mlongo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 11:51:13 by alessiolong       #+#    #+#             */
-/*   Updated: 2023/11/09 14:49:21 by abuonomo         ###   ########.fr       */
+/*   Updated: 2023/11/11 10:52:07 by mlongo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,28 @@ void	tree_node_operator(t_token *token_lst, t_tree **tree)
 	(*tree)->right = tree_create(token_lst, SIMPLE_CMD);
 }
 
+t_tree	*tree_simple_cmd(t_tree *tree, t_token *token_lst)
+{
+	if (verify_parenthesis(token_lst))
+	{
+		tree->type = PARENTHESI;
+		tree->content = parenthesis_node(token_lst);
+		return (tree);
+	}
+	tree->type = SIMPLE_CMD;
+	tree->content = simple_cmd_redirections((token_lst));
+	simple_cmd(token_lst, tree->content);
+	return (tree);
+}
+
+void	initialize_tree(t_tree *tree)
+{
+	tree->content = NULL;
+	tree->left = NULL;
+	tree->right = NULL;
+	tree->prev = NULL;
+}
+
 t_tree	*tree_create(t_token *token_lst, t_tree_enum calling)
 {
 	t_tree	*tree;
@@ -30,23 +52,9 @@ t_tree	*tree_create(t_token *token_lst, t_tree_enum calling)
 	if (!token_lst)
 		return (NULL);
 	tree = (t_tree *)malloc(sizeof(t_tree));
-	tree->content = NULL;
-	tree->left = NULL;
-	tree->right = NULL;
-	tree->prev = NULL;
+	initialize_tree(tree);
 	if (calling == SIMPLE_CMD)
-	{
-		if (verify_parenthesis(token_lst))
-		{
-			tree->type = PARENTHESI;
-			tree->content = parenthesis_node(token_lst);
-			return (tree);
-		}
-		tree->type = SIMPLE_CMD;
-		tree->content = simple_cmd_redirections((token_lst));
-		simple_cmd(token_lst, tree->content);
-		return (tree);
-	}
+		return (tree_simple_cmd(tree, token_lst));
 	else if (calling == OP)
 	{
 		while (token_lst->prev != NULL)
